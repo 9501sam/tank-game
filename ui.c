da852985c24c63e0478248be9c53e92b7dcb3770
TsengHsiHuang <9501sam@gmail.com>
2022-06-11 23:38:30 +0800

    modify tanks infomation display

--- ui.c
+++ ui.c
@@ -12,7 +12,7 @@
 #define ERASE_BLOCK(window, pos_y, pos_x) \
     PRINT_CH(window, pos_y, pos_x, ' ')
 
-WINDOW *win_game;
+WINDOW *win_game, *win_info, *win_mytank_info;
 
 static void attron_tank(int id)
 {
@@ -20,6 +20,8 @@ static void attron_tank(int id)
     int color_id = id + 1;
     init_pair(pair_id, color_id, COLOR_BLACK);
     wattron(win_game, COLOR_PAIR(pair_id));
+    wattron(win_info, COLOR_PAIR(pair_id));
+    wattron(win_mytank_info, COLOR_PAIR(pair_id));
     attron(COLOR_PAIR(pair_id));
 }
 
@@ -27,6 +29,8 @@ static void attroff_tank(int id)
 {
     int pair_id = id + 1;
     wattroff(win_game, COLOR_PAIR(pair_id));
+    wattroff(win_info, COLOR_PAIR(pair_id));
+    wattroff(win_mytank_info, COLOR_PAIR(pair_id));
     attroff(COLOR_PAIR(pair_id));
 }
 
@@ -35,25 +39,43 @@ void init_ui(void)
     initscr();
     curs_set(0);
     noecho();
-    nodelay(win_game, TRUE);
     start_color();
 
-    int starty, startx, height, width;
-    height = MAP_HEIGHT;
-    width = MAP_WIDTH * 2;
-    starty = (LINES - height) / 2;
-    startx = (COLS - width) / 2;
+    // win_game
+    int game_starty, game_startx, game_height, game_width;
+    game_height = MAP_HEIGHT;
+    game_width = MAP_WIDTH * 2;
+    game_starty = (LINES - game_height) / 2;
+    game_startx = (COLS - game_width) / 2 - 20;
+    win_game = newwin(game_height, game_width, game_starty, game_startx);
+    keypad(win_game, TRUE);
 
-    win_game = newwin(height, width, starty, startx);
+    // win_info
+    int info_starty, info_startx, info_height, info_width;
+    info_height = MAX_USERS + 2;
+    info_width = 40;
+    info_starty = game_starty + 8;
+    info_startx = game_startx + game_width + 1;
+    win_info = newwin(info_height, info_width, info_starty, info_startx);
+    box(win_info, 0, 0);
+
+    // win_mytank_info
+    int mtkinfo_starty, mtkinfo_startx, mtkinfo_height, mtkinfo_width;
+    mtkinfo_height = 7;
+    mtkinfo_width = 40;
+    mtkinfo_starty = game_starty;
+    mtkinfo_startx = game_startx + game_width + 1;
+    win_mytank_info = newwin(mtkinfo_height, mtkinfo_width, mtkinfo_starty, mtkinfo_startx);
+    box(win_mytank_info, 0, 0);
+    mvwprintw(win_mytank_info, 0, 0, "move: arrow keys, shoot: x, refill: z ");
 
     print_all_walls();
     print_tank(&my_tank);
 
     refresh();
     wrefresh(win_game);
-
-    keypad(stdscr, TRUE);
-    keypad(win_game, TRUE);
+    wrefresh(win_info);
+    wrefresh(win_mytank_info);
 }
 
 void deinit_ui(void)
@@ -65,14 +87,32 @@ void print_tank_info(const tank *tk)
 {
     int id = tk->id;
     attron_tank(id);
-    mvprintw(tk->id, 0, "player %d: (%d,%d), hp=%d, bullets=%d", tk->id + 1,
+    mvwprintw(win_info, tk->id + 1, 1,
+            "player %d: (%d,%d), hp=%d, bullets=%d", tk->id + 1,
             tk->x, tk->y, tk->hp, tk->nblts);
+    if (id == my_tank.id) {
+        mvwprintw(win_mytank_info, 2, 1, "hp:");
+        for (int i = 0; i < DEFAULT_HP; i++) {
+            if (i < my_tank.hp)
+                PRINT_BLOCK(win_mytank_info, 2, i * 2 + 3);
+            else
+                ERASE_BLOCK(win_mytank_info, 2, i * 2 + 3);
+        }
+        mvwprintw(win_mytank_info, 4, 1, "bullets:");
+        for (int i = 0; i < NUM_BULLETS; i++) {
+            if (i < my_tank.nblts)
+                PRINT_BLOCK(win_mytank_info, 4, i * 2 + 5);
+            else
+                ERASE_BLOCK(win_mytank_info, 4, i * 2 + 5);
+        }
+    }
     attroff_tank(id);
 }
 
 void erase_tank_info(const tank *tk)
 {
-    mvprintw(tk->id, 0, "                                                ");
+    mvwprintw(win_info,
+            tk->id + 1, 1, "                                   ");
 }
     
 void print_tank(const tank *tk)
@@ -117,13 +157,14 @@ void print_all_walls(void)
             if (map[i][j] == BLOCK_WALL) {
                 PRINT_BLOCK(win_game, i, j);
             }
-    // box(win_game, 0, 0);
 }
 
 void refresh_screen(void)
 {
     refresh();
     wrefresh(win_game);
+    wrefresh(win_info);
+    wrefresh(win_mytank_info);
 }
 
 input_t get_input(void)
